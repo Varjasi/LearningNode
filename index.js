201ad3ddd741a0529e96488174d73d42dfe2b8f2
Varjasi <teodorvarjasi.puskas@gmail.com>
2024-01-17 12:46:20 +0100

    update 20

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Szia");
+
+var a = 3;
+var b = 5;
+var c = a+b;
+
+function osszead(num1, num2) {
+    return num1 + num2
+    document.body.children[0].innerHTML = num1 + num1
+}
+
+var a = 3;
+var b = 5;
+var c = osszead(a,b);
+
+console.log(c)
